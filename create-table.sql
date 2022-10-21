82d72bb40238749bba4c0cf3d658273f13863140
Alexius337 <almyro@gmail.com>
2022-10-20 22:30:05 -0700

    dropping tables

--- create-table.sql
+++ create-table.sql
@@ -1,10 +1,16 @@
 CREATE DATABASE cat_app;
+
 use cat_app;
+
 CREATE TABLE cats
 	(
 		name VARCHAR(100),
         age int
 	);
+
 SHOW TABLES;
 SHOW COLUMNS FROM cats;
+
 DESC cats;
+
+DROP TABLE cats;
