0dcbe47c6425d56291c2401782a826c24fcc063e
Alexius337 <almyro@gmail.com>
2022-10-22 00:06:27 -0700

    inserting data into table

---
+++ insert-into-table.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE cat_app;
+
+use cat_app;
+
+CREATE TABLE cats
+	(
+		name VARCHAR(100),
+        age int
+	);
+
+INSERT INTO cats(name, age)
+VALUES ('Jetson', 7);
+
+INSERT INTO cats(name, age)
+VALUES ('Victoria', 12);
+
+DESC cats;
+SHOW COLUMNS FROM cats;
+
