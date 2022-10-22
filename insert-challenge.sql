f0b5d1408bb2bd11ddbd37a52b7c2daccc47c530
Alexius337 <almyro@gmail.com>
2022-10-22 00:22:50 -0700

    insert challenge

---
+++ insert-challenge.sql
@@ -0,0 +1,19 @@
+CREATE TABLE people
+	(
+		first_name VARCHAR(20),
+        last_name VARCHAR(20),
+        age int
+	);
+
+SHOW TABLES;
+DESC people;
+
+INSERT INTO people(first_name, last_name, age)
+VALUES ('Tina', 'Belcher', 13);
+
+SELECT * FROM people;
+
+INSERT INTO people(age, first_name, last_name)
+VALUES (42, 'Bob', 'Belcher');
+
+SELECT * FROM people;
