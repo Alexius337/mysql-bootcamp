c37ec55b2242a7885b3b2128dc25e06ab03fae9b
Alexius337 <almyro@gmail.com>
2022-10-22 22:29:47 -0700

    intro to aliases

--- examples/select-practice.sql
+++ examples/select-practice.sql
@@ -9,3 +9,7 @@ SELECT * FROM cats WHERE age=4;
 SELECT * FROM cats WHERE name='Egg';
 SELECT * FROM cats WHERE name='EGG';
 
+SELECT cat_id AS id, name FROM cats;
+
+SELECT name AS 'cat name' FROM cats;
+
---
+++ exercises/select-exercises.sql
@@ -0,0 +1,8 @@
+SELECT cat_id FROM cats;
+
+SELECT name, breed FROM cats;
+
+SELECT name, age FROM cats WHERE breed='Tabby';
+
+SELECT cat_id, age FROM cats WHERE cat_id=age;
+
