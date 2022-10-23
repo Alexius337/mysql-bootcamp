4d0b364fdf5128402d4fe4329250fde2e8610aa8
Alexius337 <almyro@gmail.com>
2022-10-22 22:40:47 -0700

    UPDATE challenge

---
+++ exercises/update-exercises.sql
@@ -0,0 +1,14 @@
+UPDATE cats SET name='Jack'
+WHERE name='Jackson';
+
+SELECT * FROM cats;
+
+UPDATE cats SET breed='British Shorthair'
+WHERE name='Ringo';
+
+SELECT * FROM cats;
+
+UPDATE cats SET age=12
+WHERE breed='Maine Coon';
+
+SELECT * FROM cats;
